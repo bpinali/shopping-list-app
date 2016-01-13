1d73309fa005382d70ffbf2868514325fa7a28b7
Ben Pinali <ben@benpinali.com>
2016-01-12 18:35:23 -0600

    Spacing changes in CSS, basic JS outline

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+/* Function to add items */
+function addItem() {}
+
+
+/* Function to check items in the list */
+function checkItem() {}
+
+
+/* Function to remove items from the list */
+function removeItem() {}
+
+
+/* Function to calculate total for budgeted and actual columns */
+function calculateTotal() {}
+
+
+/* Function to clear the list of all items */
+function clearList() {}
+
+$(document).ready(function () {
+    /* on click on the ".add-item" button activate function called addItem()  */
+
+    /* on click on the ".checkbox" button activate function called checkItem()  */
+
+    /* on click on the ".delete" button activate function called removeItem()  */
+
+    /* on click on the "" button activate function called clearList()  */
+
+    /* upon entry or modification of price input value activate function called calculateTotal()  */
+});
