52ff8decc61e81fd53645ce8c50bd9ef5447f198
Ben Pinali <ben@benpinali.com>
2016-01-19 19:23:39 -0600

    Removed total budget and total actual columns, their CSS rules, and associated JS.

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,6 @@
-var totalBudgetedPrice = 0;
-var totalActualPrice = 0;
-
-/* Function to calculate total for budgeted and actual columns */
-function calculateTotalBudgetedPrice(totalBudgetedPrice, updateBudgetedPrice) {
-    console.log(totalBudgetedPrice);
-    console.log(updateBudgetedPrice);
-    if (updateBudgetedPrice != 0) {
-        totalBudgetedPrice = totalBudgetedPrice + updateBudgetedPrice;
-    }
-    $('#budget-total').text("$" + parseFloat(totalBudgetedPrice).toFixed(2));
-}
-
-function calculateTotalActualPrice(totalActualPrice, updateActualPrice) {
-    console.log(totalActualPrice);
-    console.log(updateActualPrice);
-    if (updateActualPrice != 0) {
-        totalActualPrice = totalActualPrice + updateActualPrice;
-    }
-    $('#actual-total').text("$" + parseFloat(totalActualPrice).toFixed(2));
-}
-
-
-
 /* Function to add items */
 function addItem() {
     var itemName = $('#newItem').val();
-    var itemBudgetPrice = $('#budgetPrice').val();
-    var itemActualPrice = $('#actualPrice').val();
     // alert("add item value" + itemName + itemBudgetPrice + itemActualPrice);
 
     if (itemName.length === 0) {
@@ -35,21 +9,13 @@ function addItem() {
     } else {
 
         //dynamicaly create one row inside the shopping list
-        var row = $('<li><button class="checkbox"><i class="fa fa-check"></i></button>' + itemName + '<button class="delete"><i class="fa fa-times"></i></button><input class="actual-price" type="text" value="' + parseFloat(itemActualPrice).toFixed(2) + '"><input class="budget-price" type="text" value="' + parseFloat(itemBudgetPrice).toFixed(2) + '"></li>');
+        var row = $('<li><button class="checkbox"><i class="fa fa-check"></i></button>' + itemName + '<button class="delete"><i class="fa fa-times"></i></button></li>');
 
         //add each row to the previous ones
         $('.list-items').append(row);
 
-        if (itemBudgetPrice != '') {
-            calculateTotalBudgetedPrice(totalBudgetedPrice, itemBudgetPrice);
-        }
-        if (itemActualPrice != '') {
-            calculateTotalActualPrice(totalActualPrice, itemActualPrice);
-        }
         //empty the input box after submit by reseting the value
         itemName = $('#newItem').val('');
-        itemBudgetPrice = $('#budgetPrice').val('');
-        itemActualPrice = $('#actualPrice').val('');
     }
 
 }
@@ -75,8 +41,6 @@ function removeItem() {
 /* Function to clear the list of all items */
 function clearList() {
     $('.list-items').empty();
-    $('#budget-total').html("$0.00");
-    $('#actual-total').html("$0.00");
 }
 
 $(document).ready(function () {
@@ -97,5 +61,3 @@ $(document).on('click', '.checkbox', checkItem);
 
 /* on click on the ".delete" button activate function called removeItem()  */
 $(document).on('click', '.delete', removeItem);
-
-/* upon entry or modification of price input value activate function called calculateTotal()  */
